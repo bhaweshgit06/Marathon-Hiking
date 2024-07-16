c0441bd237e62c76ae4aa052e76284a2b37d8177
bhawesh-a_ola <bhawesh.a@olacabs.com>
2024-07-17 01:01:06 +0530

    check-routes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import MapComponent from '../Components/customMap.js';
 
 const Home = () => {
-  
+  const navigate = useNavigate();
+
+  const redirectToLogin = () => {
+    navigate('/login');
+  };
+
   return (
     <div>
       <h1>Home Page</h1>
+      <div onClick={redirectToLogin}>Redirect to login</div>
     </div>
   );
 };
